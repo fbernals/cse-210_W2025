d91b063cc314b210acbb4344a3d4538b89f33124
Fabian Bernal <fbernals@aol.com>
2025-01-08 09:49:18 -0700

    Prep 3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Enter a number to guess: ");
+        int numberToGuess = int.Parse(Console.ReadLine());
+        int guessedNumber = 0;
+        
+        while (guessedNumber != numberToGuess) {
+            Console.Write("Enter guessed number: ");
+            guessedNumber = int.Parse(Console.ReadLine());
+            
+            if (guessedNumber < numberToGuess) {
+                    Console.WriteLine("Guess higher.");
+            }
+            else if (guessedNumber > numberToGuess) {
+                Console.WriteLine("Guess lower");
+            }
+            else if (guessedNumber == numberToGuess) {
+                Console.WriteLine("You guessed it!");
+            }
+        
+    } 
     }
 }
