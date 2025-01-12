c3005004f74eb7fe7cf934bb817be685a83ff551
Fabian Bernal <fbernals@aol.com>
2025-01-11 19:53:09 -0700

    Prep 4 lists with stretch challenges

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+        int sum = 0;
+        float average;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        List<int> sortedNumbers = new List<int>();
+        do {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0){
+                numbers.Add(number);
+            }
+            
+        } while (number != 0);
+        
+        foreach (int item in numbers){
+            Console.WriteLine(item);
+            sum += item;
+            
+            
+            if (item > max){
+                max = item;
+            }
+            
+            if (item < min && item > 0){
+                min = item;
+            }
+            
+        }
+
+        sortedNumbers = numbers.OrderBy(x => x).ToList();
+        average = (float)sum / numbers.Count;
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
+        Console.WriteLine($"The smallest positive number is {min}");
+        Console.WriteLine("The numbers in ascending order are: ");
+        foreach (int num in sortedNumbers)
+        {
+            Console.WriteLine(num);
+        }
+            
     }
 }
