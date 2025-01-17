be6082ec859bf7af9c61b7a15ce156a76cf47fde
Fabian Bernal <fbernals@aol.com>
2025-01-17 07:01:51 -0700

    Learn02 job and resume.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data analyst";
+        job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
