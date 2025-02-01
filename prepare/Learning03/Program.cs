3510fb2f9f22550727faf2aea575588c894024c3
fbernals@aol.com <fbernals@aol.com>
2025-02-01 09:58:25 -0700

    Learning 3 finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        f1.SetNumerator(3);
+        f1.SetDenominator(4);
+        Fraction f2 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
     }
 }
